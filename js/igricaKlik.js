c2368670d22ec6ae34ef2cfab301177ab4f0b4ea
lenapuletic <puleticlena@gmail.com>
2020-12-06 22:01:49 +0100

    dodate igrice klik i reakcija

--- js/igricaKlik.js
+++ js/igricaKlik.js
@@ -1,36 +1,14 @@
-// var sekunde = 10;
+document.getElementById('start').addEventListener("click", odbrojavanje);
+//document.getElementById('start').addEventListener("click", brojanje);
+var sekunde = 10;
 
-// setInterval( function(){
-//     sekunde--;
-
-//     if(sekunde >= 0){
-//         document.getElementById('brSek').innerHTML = sekunde;
-//     }
-//     if(sekunde === 0){
-//         document.getElementById('brSek').innerHTML = "Vreme je isteklo";
-//     }
-// }, 1000);
-
-
-
-
-
-
-var funkcijaBrojanja = (function () {
-
-    var brojac = 0;
-    return function () {return brojac += 1;}
-})();
-
-
-
-function brojanje(){
-    document.getElementById('broj').innerHTML = funkcijaBrojanja(); 
+function odbrojavanje(){
     setInterval( function(){
         sekunde--;
             
         if(sekunde >= 0){
             document.getElementById('brSek').innerHTML = sekunde;
+            
         }
             
         if(sekunde === 0){
@@ -41,9 +19,12 @@ function brojanje(){
      }, 1000);
 }
 
-var sekunde = 10;
+var funkcijaBrojanja = (function () {
 
+    var brojac = 0;
+    return function () {return brojac += 1;}
+})();
 
-
-
-
+function brojanje(){
+    document.getElementById('broj').innerHTML = funkcijaBrojanja();
+}
